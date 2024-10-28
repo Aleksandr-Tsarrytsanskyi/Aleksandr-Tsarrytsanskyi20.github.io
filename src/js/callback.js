d002b4b477e1771c3c38897c03db6b1351f751f3
Alexksandr Tsarrytsanskyi <Carekjustin2009@gmail.com>
2024-10-28 13:47:39 +0300

    init

---
+++ src/js/callback.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const request = document.querySelector("#request");
+
+    request.classList.add("show_request");
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const callback = document.querySelector(".callback input[type='submit']");
-
+    const call = document.querySelector("#call");
+    const order = document.querySelector("#order");
+    const userApi = document.querySelector("#userApi");
+    
     callback.addEventListener("click", function(event) {
         event.preventDefault();
     })
+    order.addEventListener("click", function(event) {
+        event.preventDefault();
+        call.classList.add("show_callback")
+    })
+
+    userApi.addEventListener("submit", function(event) {
+        event.preventDefault();
+        document.location.assign("./request.html");
+    })
+
 })
