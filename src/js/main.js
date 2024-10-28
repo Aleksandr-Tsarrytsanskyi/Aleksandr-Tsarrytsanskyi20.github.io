6a90ddfcd2082710b37bb0ba25b7f09ff2f2248e
Alexksandr Tsarrytsanskyi <Carekjustin2009@gmail.com>
2024-10-28 19:21:35 +0300

    init

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const userApi = document.querySelector("#userApi");
     const more = document.querySelector("#investments .more");
     const items = document.querySelectorAll(".item")
+    const  closeForm  = document.querySelector("#call .close")
+    const request = document.querySelector("#request");
     items.forEach(element => {
         element.addEventListener("click", function(event) {
             event.preventDefault();
+            disabledScroll();
             call.classList.add("show_callback")
         })
     });
@@ -14,20 +17,35 @@ document.addEventListener("DOMContentLoaded", function() {
     order.addEventListener("click", function(event) {
         event.preventDefault();
         call.classList.add("show_callback")
+        call.classList.remove("hide_callback");
         disabledScroll();
     })
 
     userApi.addEventListener("submit", function(event) {
         event.preventDefault();
-        document.location.assign("./request.html");
+       
+        call.style.display = "none"
+        request.classList.add("show_request");
+    })
+
+    closeForm.addEventListener("click", function() {
+        anabladeScroll();
+        call.classList.remove("show_callback");
+        call.classList.add("hide_callback");
     })
 
     function disabledScroll() {
         document.body.style.cssText = `
         overflow: hidden;
         position: relative;
-        height: 100vh;
-        width: 100vw
+        height: 100vh;   `
+    }
+
+    function anabladeScroll() {
+        document.body.style.cssText = `
+        overflow: visible;
+        position: relative;
+        height: 100vh;   
         `
     }
 
