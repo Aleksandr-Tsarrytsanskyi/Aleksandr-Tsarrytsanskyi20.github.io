6ff331fbb7b8be7954426cc06d90ca8939330ed7
Alexksandr Tsarrytsanskyi <Carekjustin2009@gmail.com>
2024-10-28 15:45:06 +0300

    init

--- src/js/call.js
+++ src/js/call.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() { 
     const call = document.querySelector("#call");
     const userApi = document.querySelector("#userApi");
-
+    
     call.classList.add("show_callback1")
 
     userApi.addEventListener("submit", function(event) {
