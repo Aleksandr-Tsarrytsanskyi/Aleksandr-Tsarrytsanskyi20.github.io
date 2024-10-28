9598dce51b99fe207bad1402e3429028ef98cf75
Alexksandr Tsarrytsanskyi <Carekjustin2009@gmail.com>
2024-10-28 18:14:16 +0300

    init

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,31 @@ document.addEventListener("DOMContentLoaded", function() {
     const userApi = document.querySelector("#userApi");
     const more = document.querySelector("#investments .more");
     const items = document.querySelectorAll(".item")
-    
     items.forEach(element => {
         element.addEventListener("click", function(event) {
             event.preventDefault();
-            document.location.assign("./callback.html");
+            call.classList.add("show_callback")
         })
     });
 
     order.addEventListener("click", function(event) {
         event.preventDefault();
         call.classList.add("show_callback")
+        disabledScroll();
     })
 
     userApi.addEventListener("submit", function(event) {
         event.preventDefault();
         document.location.assign("./request.html");
     })
+
+    function disabledScroll() {
+        document.body.style.cssText = `
+        overflow: hidden;
+        position: relative;
+        height: 100vh;
+        width: 100vw
+        `
+    }
 
 })
